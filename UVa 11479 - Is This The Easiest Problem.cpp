c437b26f877820168e9d247518a91abc4a8889ed
Redwan Ahmed Adib <42737943+redwan247@users.noreply.github.com>
2019-04-24 17:40:16 +0600

    Update UVa 11479 - Is This The Easiest Problem.cpp

--- UVa 11479 - Is This The Easiest Problem.cpp
+++ UVa 11479 - Is This The Easiest Problem.cpp
@@ -7,9 +7,7 @@ int main()
     scanf("%lld", &test);
     for(int i=1; i<=test; i++){
         scanf("%lld %lld %lld", &a, &b, &c);
-        if(a==0 || b==0 || c==0)
-            printf("Case %d: Invalid\n", i);
-        else if(a<0 || b<0 || c<0)
+        else if(a<=0 || b<=0 || c<=0)
             printf("Case %d: Invalid\n", i);
         else if(a==b && b==c)
             printf("Case %d: Equilateral\n", i);
